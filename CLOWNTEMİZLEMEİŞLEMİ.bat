b8dbf3cb97c94578d7159d1f3246bdab22d3cd72
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2023-03-22 12:29:09 +0300

    Create CLOWNTEMİZLEMEİŞLEMİ.bat

---
+++ CLOWNTEMİZLEMEİŞLEMİ.bat
@@ -0,0 +1,9 @@
+@ECHO off
+echo :fh4dangersign: Clown algılandı!!! :fh4dangersign: 
+echo Temizleme işlemi başlıyor...
+echo :fh4upgrade: Clown temizlenip alan tamir ediliyor... :fh4upgrade: 
+echo :fh4helmet: Kalkanlar aktif! :fh4helmet: 
+echo :fh4superstar: Son kontroller.... :fh4superstar: 
+echo :fh4skill: :fh4: Temizleme başarılı! :fh4: :fh4skill: 
+echo Bu programı kullandığınız için teşekkür ederiz.
+pause 5
